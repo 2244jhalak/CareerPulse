ae7709f7228ed7ad64efa1a237630e8a3cf5ed0d
Jhalak <stdjhalak@gmail.com>
2024-05-11 18:14:02 +0600

    DatePicker added

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,13 +1,45 @@
+import { useContext, useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../provider/AuthProvider";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 
 const JobDetails = () => {
+    
+    const [startDate, setStartDate] = useState(new Date());
+    const {user}=useContext(AuthContext);
     const job=useLoaderData();
+    
+    const {_id,Name,Title,Deadline,Salary,Applicants,Image,email,Description} = job;
     console.log(job);
-    const {_id,Name,Title,Date,Deadline,Salary,Applicants,Image,email,Description} = job;
+    
+    const handleSubmit =async e=>{
+        e.preventDefault();
+        const form=e.target;
+        const userName=form.name.value;
+        const userEmail=form.email.value;
+        const jobId=_id;
+        const title=Title;
+        const companyName=Name;
+        const companyEmail=email;
+        
+        const jobsDeadline=Deadline;
+        const myDeadline=startDate
+        const salary=Salary;
+        const applicants=Applicants;
+        const img=Image;
+        const description=Description;
+        const appliedData={
+            userName,userEmail,jobId,title,companyName,companyEmail,jobsDeadline,myDeadline,salary,applicants,img,description
+        }
+        console.log(appliedData);
+    }
+
   
     return (
-        <div>
+        <div>    
             
 
     <div className="container flex flex-col px-6 py-10 mx-auto space-y-6 lg:h-[32rem] lg:py-16 lg:flex-row lg:items-center">
@@ -26,31 +58,28 @@ const JobDetails = () => {
   <section className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
     <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">Application settings</h2>
 
-    <form>
+    <form onSubmit={handleSubmit}>
         <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
             <div>
                 <label className="text-gray-700 dark:text-gray-200" >Username</label>
-                <input id="username" type="text" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
+                <input id="username" type="text" name="name" defaultValue={user?.displayName} className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" disabled/>
             </div>
 
             <div>
                 <label className="text-gray-700 dark:text-gray-200" >Email Address</label>
-                <input id="emailAddress" type="email" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
+                <input id="emailAddress" name="email" defaultValue={user?.email} type="email" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" disabled/>
+            </div>
+            <div>
+                <label className="text-gray-700 dark:text-gray-200" >Deadline</label>
+                <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+                
             </div>
 
-            <div>
-                <label className="text-gray-700 dark:text-gray-200" >Password</label>
-                <input id="password" type="password" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
-            </div>
-
-            <div>
-                <label className="text-gray-700 dark:text-gray-200" >Password Confirmation</label>
-                <input id="passwordConfirmation" type="password" className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"/>
-            </div>
+           
         </div>
 
         <div className="flex justify-end mt-6">
-            <input className="py-2.5 text-center cursor-pointer leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600" value="Submit"/>
+            <input className="py-2.5 px-5 text-center cursor-pointer leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600" type="submit" value="Submit"/>
         </div>
     </form>
 </section>
@@ -68,7 +97,7 @@ const JobDetails = () => {
         </div>
 
         <div className="flex items-center justify-center w-full h-96 lg:w-1/2">
-            <img className="object-cover w-full h-full max-w-2xl rounded-md" src={Image} alt="glasses photo"/>
+            <img className="object-cover w-full h-full max-w-2xl rounded-md" src={Image} alt="content photo"/>
         </div>
     </div>
 
